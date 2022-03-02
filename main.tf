c9f1a7970be92c47380e371930f81f5954d0cadd
brisido <roidsz.rx@gmail.com>
2022-03-02 18:27:16 -0300

    adding sqs - dlq settings

---
+++ main.tf
@@ -0,0 +1,59 @@
+locals {
+  sqs_queue = {
+    sqs1 = {
+      name = "fila-teste-a_queue"
+    }
+    sqs2 = {
+      name                       = "fila-teste-b_queue"
+      delay_seconds              = 5
+      max_message_size           = 131072
+      message_retention_seconds  = 1440
+      receive_wait_time_seconds  = 2
+      visibility_timeout_seconds = 15
+    }
+    sqs3 = {
+      name = "fila-teste-c_queue"
+      dlq = {
+        enable = true
+      }
+    }
+    sqs4 = {
+      name                       = "fila-teste-d_queue"
+      delay_seconds              = 5
+      max_message_size           = 131072
+      message_retention_seconds  = 1440
+      receive_wait_time_seconds  = 2
+      visibility_timeout_seconds = 15
+      dlq = {
+        enable                     = true
+        retries                    = 10
+        delay_seconds              = 1
+        max_message_size           = 196608
+        message_retention_seconds  = 86400
+        receive_wait_time_seconds  = 5
+        visibility_timeout_seconds = 25
+      }
+    }
+    sqs5 = {
+      name                       = "fila-teste-e_queue"
+      delay_seconds              = 5
+      max_message_size           = 131072
+      message_retention_seconds  = 1440
+      receive_wait_time_seconds  = 2
+      visibility_timeout_seconds = 15
+      dlq = {
+        enable                     = true
+        retries                    = 10
+        delay_seconds              = 1
+        max_message_size           = 196608
+        message_retention_seconds  = 86400
+        receive_wait_time_seconds  = 5
+        visibility_timeout_seconds = 25
+      }
+    #   subscription = {
+    #     protocol = "sqs"
+    #     name = "nome_do_topico"
+    #   }
+    }
+  }
+}
---
+++ sqs.tf
@@ -0,0 +1,52 @@
+# CREATE SQS QUEUE RESOURCE
+resource "aws_sqs_queue" "queue" {
+  for_each                   = local.sqs_queue
+  name                       = each.value.name
+  delay_seconds              = try(each.value.delay_seconds, 0)
+  max_message_size           = try(each.value.max_message_size, 262144)
+  message_retention_seconds  = try(each.value.message_retention_seconds, 345600)
+  receive_wait_time_seconds  = try(each.value.receive_wait_time_seconds, 0)
+  visibility_timeout_seconds = try(each.value.visibility_timeout_seconds, 30)
+  redrive_policy             = try(each.value.dlq.enable == true ? jsonencode({
+                                 deadLetterTargetArn = aws_sqs_queue.dlq[each.key].arn
+                                 maxReceiveCount     = try(each.value.dlq.retries, 5)
+                               }) : null, null)
+}
+
+# CREATE SQS DLQ RESOURCE
+resource "aws_sqs_queue" "dlq" {
+  for_each                   = local.sqs_queue
+  name                       = replace(each.value.name, "queue", "dlq")
+  delay_seconds              = try(each.value.dlq.delay_seconds, 0)
+  max_message_size           = try(each.value.dlq.max_message_size, 262144)
+  message_retention_seconds  = try(each.value.dlq.message_retention_seconds, 1209600)
+  receive_wait_time_seconds  = try(each.value.dlq.receive_wait_time_seconds, 0)
+  visibility_timeout_seconds = try(each.value.dlq.visibility_timeout_seconds, 30)
+}
+
+# CREATE POLICY FOR SQS QUEUE RESOURCE
+resource "aws_sqs_queue_policy" "sqs_policy" {
+  for_each  = local.sqs_queue
+  queue_url = aws_sqs_queue.queue[each.key].id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.queue[each.key].arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.sns.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
